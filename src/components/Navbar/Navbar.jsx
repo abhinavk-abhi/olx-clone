eb79fec42df0c535257a5ac3978afcad8187b710
abhinavk-abhi <abhinavkottyadi234@gmail.com>
2025-12-20 20:25:19 +0530

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,46 +9,59 @@ const Navbar = (props) => {
   const {toggleModal} = props
   return (
     <div>
-    <nav className="fixed z-50 w-full p-2 pl-3 pr-3 shadow-md bg-slate-100 border-b-4 border-b-white flex items-center">
+    <nav className="fixed z-50 w-full p-2 px-3 shadow-md bg-slate-100 border-b-4 border-b-white flex items-center gap-3">
 
+  {/* Logo */}
+  <img src={logo} alt="Logo" className="w-12 flex-shrink-0" />
 
-      <img src={logo} alt="Logo" className="w-12" />
+  {/* Location Search */}
+  <div className="relative ml-3 flex-shrink-0">
+    <img src={search} alt="" className="absolute top-4 left-2 w-5" />
+    <input
+      type="text"
+      placeholder="Search city, area or locality..."
+      className="w-[120px] sm:w-[180px] md:w-[240px] p-3 pl-8 pr-8 border-2 border-black rounded-md focus:outline-none focus:border-teal-300"
+    />
+    <img src={arrow} alt="" className="absolute top-4 right-3 w-5 cursor-pointer" />
+  </div>
 
+  {/* Main Search (flex-grow) */}
+  <div className="relative flex-grow mx-2">
+    <input
+      placeholder="Find Cars, Mobile Phones, and More..."
+      className="w-full p-3 border-2 border-black rounded-md focus:outline-none focus:border-teal-300"
+      type="text"
+    />
+    <div
+      style={{ backgroundColor: '#002f34' }}
+      className="flex justify-center items-center absolute top-0 right-0 h-full w-12 rounded-e-md"
+    >
+      <img className="w-5 filter invert" src={searchwt} alt="Search" />
+    </div>
+  </div>
 
-      <div className="relative location-search ml-5">
-        <img src={search} alt="" className="absolute top-4 left-2 w-5" />
-        <input
-          type="text"
-          placeholder="Search city, area or locality..."
-          className="w-[50px] sm:w-[150px] md:w-[250px] lg:w-[270px] p-3 pl-8 pr-8 border-2 border-black rounded-md focus:outline-none focus:border-teal-300"
-        />
-        <img src={arrow} alt="" className="absolute top-4 right-3 w-5 cursor-pointer" />
-      </div>
+  {/* Right Section */}
+  <div className="flex items-center gap-5 flex-shrink-0">
 
+    <div className="flex items-center gap-1 cursor-pointer">
+      <p className="font-bold">English</p>
+      <img src={arrow} alt="" className="w-5" />
+    </div>
 
-      <div className="ml-5 mr-2 relative w-full main-search">
-        <input
-          placeholder="Find Cars, Mobile Phones, and More..."
-          className="w-full p-3 border-2 border-black rounded-md focus:outline-none focus:border-teal-300"
-          type="text"
-        />
+    <p
+      onClick={toggleModal}
+      className="font-bold cursor-pointer hover:text-teal-600"
+    >
+      Login
+    </p>
 
-        <div
-          style={{ backgroundColor: '#002f34' }}
-          className="flex justify-center items-center absolute top-0 right-0 h-full rounded-e-md w-12"
-        >
-          <img className="w-5 filter invert" src={searchwt} alt="Search" />
-        </div>
-      </div>
+    <p className="font-bold cursor-pointer border-2 border-black rounded-full px-4 py-1">
+      Sell
+    </p>
 
-      <div className='mx-1 sm:ml-5 sm:mr-5 relative lang'>
-        <p className='font-bold mr-3'>English</p>
-        <img src={arrow} alt="" className='w-5 cursor-pointer' />
-      </div>
+  </div>
+</nav>
 
-     <p onClick={toggleModal}>Login</p> 
-
-    </nav>
     </div>
   )
 }
